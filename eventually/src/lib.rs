f1f9a85758573aa36889326267dec1d662028482
Danilo Cianfrone <danilocianfr@gmail.com>
2019-12-09 00:13:11 +0100

    feat(eventually): export minimalistic API

--- eventually/src/lib.rs
+++ eventually/src/lib.rs
@@ -1,5 +1,11 @@
-#![allow(warnings, dead_code)]
+#![allow(warnings)]
 
 pub mod aggregate;
 pub mod command;
 pub mod store;
+
+pub use {
+    aggregate::Aggregate,
+    command::Handler as CommandHandler,
+    store::{ReadStore, WriteStore},
+};
