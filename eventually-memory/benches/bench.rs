ab32ba14379a8468b9b71cba0f35bf9022f1a620
Danilo Cianfrone <danilocianfr@gmail.com>
2019-12-09 00:14:24 +0100

    chore: more bench tests for MemoryStore

--- eventually-memory/benches/bench.rs
+++ eventually-memory/benches/bench.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use futures::stream::StreamExt;
@@ -20,29 +22,6 @@ fn read_event_stream(store: &MemoryStore<&'static str, Event>, source_id: &'stat
     tokio_test::block_on(store.stream(source_id, 0).collect::<Vec<Event>>());
 }
 
-fn insert_new_events_concurrently_4_threads() {
-    let store = std::sync::Arc::new(std::sync::Mutex::new(
-        MemoryStore::<&'static str, Event>::default(),
-    ));
-
-    (0..4)
-        .map(|_id| {
-            std::thread::spawn({
-                let clone = store.clone();
-                move || {
-                    let mut store = clone.lock().unwrap();
-                    tokio_test::block_on(store.append(
-                        "benchtest",
-                        0,
-                        vec![Event::A, Event::B, Event::C],
-                    ))
-                    .unwrap();
-                }
-            })
-        })
-        .for_each(|handle| handle.join().unwrap());
-}
-
 fn insert_elements(store: &mut MemoryStore<&'static str, Event>, name: &'static str, num: usize) {
     tokio_test::block_on(store.append(
         name,
@@ -55,21 +34,59 @@ fn insert_elements(store: &mut MemoryStore<&'static str, Event>, name: &'static 
 fn benchmark(c: &mut Criterion) {
     let mut store = MemoryStore::<&'static str, Event>::default();
 
-    insert_elements(&mut store, "benchtest3", 3);
     insert_elements(&mut store, "benchtest10", 10);
     insert_elements(&mut store, "benchtest100", 100);
-    insert_elements(&mut store, "benchtest1000", 1000);
+    insert_elements(&mut store, "benchtest1_000", 1000);
+    insert_elements(&mut store, "benchtest10_000", 10000);
+    insert_elements(&mut store, "benchtest100_000", 100000);
+    insert_elements(&mut store, "benchtest1_000_000", 1000000);
 
-    c.bench_function("insert new events", |b| {
-        b.iter(|| insert_new_events(black_box(&mut store)))
+    c.bench_function("insert events 10", |b| {
+        b.iter(|| insert_elements(black_box(&mut store), black_box("insert_benchtest10"), 10))
     });
 
-    c.bench_function("insert new events (4 threads)", |b| {
-        b.iter(insert_new_events_concurrently_4_threads)
+    c.bench_function("insert events 100", |b| {
+        b.iter(|| insert_elements(black_box(&mut store), black_box("insert_benchtest100"), 100))
     });
 
-    c.bench_function("read stream events 3", |b| {
-        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest3")))
+    c.bench_function("insert events 1_000", |b| {
+        b.iter(|| {
+            insert_elements(
+                black_box(&mut store),
+                black_box("insert_benchtest1_000"),
+                1000,
+            )
+        })
+    });
+
+    c.bench_function("insert events 10_000", |b| {
+        b.iter(|| {
+            insert_elements(
+                black_box(&mut store),
+                black_box("insert_benchtest10_000"),
+                10000,
+            )
+        })
+    });
+
+    c.bench_function("insert events 100_000", |b| {
+        b.iter(|| {
+            insert_elements(
+                black_box(&mut store),
+                black_box("insert_benchtest100_000"),
+                100000,
+            )
+        })
+    });
+
+    c.bench_function("insert events 1_000_000", |b| {
+        b.iter(|| {
+            insert_elements(
+                black_box(&mut store),
+                black_box("insert_benchtest1_000_000"),
+                1000000,
+            )
+        })
     });
 
     c.bench_function("read stream events 10", |b| {
@@ -80,8 +97,20 @@ fn benchmark(c: &mut Criterion) {
         b.iter(|| read_event_stream(black_box(&store), black_box("benchtest100")))
     });
 
-    c.bench_function("read stream events 1000", |b| {
-        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest1000")))
+    c.bench_function("read stream events 1_000", |b| {
+        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest1_000")))
+    });
+
+    c.bench_function("read stream events 10_000", |b| {
+        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest10_000")))
+    });
+
+    c.bench_function("read stream events 100_000", |b| {
+        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest100_000")))
+    });
+
+    c.bench_function("read stream events 1_000_000", |b| {
+        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest1_000_000")))
     });
 }
 
