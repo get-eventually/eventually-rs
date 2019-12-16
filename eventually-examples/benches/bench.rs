bc30b2a33d975e533fd9ea485f4f434bbae0899e
Danilo Cianfrone <danilocianfr@gmail.com>
2019-12-16 12:43:01 +0100

    feat: add command::dispatcher::Dispatcher (#16)
    * feat(eventually): add command::dispatcher::Dispatcher impl
    
    * chore(eventually): add AggregateExt to public api
    
    * feat: add new eventually-examples crate
    
    * bench(eventually-examples): benchmarks for Dispatcher::dispatch
    
    * test(eventually-examples): add unit tests for point.rs

---
+++ eventually-examples/benches/bench.rs
@@ -0,0 +1,63 @@
+#![allow(warnings)]
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use futures::stream::StreamExt;
+
+use rand::prelude::*;
+
+use eventually::{
+    aggregate::{optional::AsAggregate, versioned::AsAggregate as VersionedAggregate},
+    command::{
+        dispatcher::Dispatcher,
+        r#static::{AsHandler, StaticHandler},
+    },
+};
+use eventually_memory::MemoryStore;
+
+type DispatcherType =
+    Dispatcher<MemoryStore<std::string::String, point::Event>, AsHandler<point::CommandHandler>>;
+
+fn dispatch(dispatcher: &mut DispatcherType, id: &'static str) {
+    let mut rng = rand::thread_rng();
+    let random: i32 = rng.gen::<i32>() % 100;
+    let choice: u8 = rng.gen::<u8>() % 4;
+
+    tokio_test::block_on(dispatcher.dispatch(match choice {
+        0 => point::Command::GoUp {
+            id: id.to_string(),
+            v: random,
+        },
+        1 => point::Command::GoDown {
+            id: id.to_string(),
+            v: random,
+        },
+        2 => point::Command::GoLeft {
+            id: id.to_string(),
+            v: random,
+        },
+        3 => point::Command::GoRight {
+            id: id.to_string(),
+            v: random,
+        },
+        _ => panic!("should not be entered"),
+    }));
+}
+
+fn benchmark(c: &mut Criterion) {
+    let store = eventually_memory::MemoryStore::<String, point::Event>::default();
+    let handler = point::CommandHandler::as_handler();
+
+    let mut dispatcher = Dispatcher::new(store, handler);
+
+    tokio_test::block_on(dispatcher.dispatch(point::Command::Register {
+        id: "benchmark".to_string(),
+    }));
+
+    c.bench_function("dispatch", |b| {
+        b.iter(|| dispatch(black_box(&mut dispatcher), black_box("benchmark")))
+    });
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
---
+++ eventually-examples/src/main.rs
@@ -0,0 +1,74 @@
+use eventually::command::{
+    dispatcher::{Dispatcher, Error},
+    r#static::StaticHandler,
+};
+
+use rand::prelude::*;
+
+type DispatchError = Error<point::EventError, point::CommandError, std::convert::Infallible>;
+
+fn main() {
+    let store = eventually_memory::MemoryStore::<String, point::Event>::default();
+    let handler = point::CommandHandler::as_handler();
+
+    let mut dispatcher = Dispatcher::new(store, handler);
+
+    let result: Result<(), DispatchError> = tokio_test::block_on(async {
+        println!(
+            "Register: {:?}",
+            dispatcher
+                .dispatch(point::Command::Register {
+                    id: "MyPoint".to_string(),
+                })
+                .await?
+        );
+
+        for _i in 0..1000 - 1 {
+            let mut rng = rand::thread_rng();
+            let random: i32 = rng.gen::<i32>() % 100;
+            let choice: u8 = rng.gen::<u8>() % 4;
+
+            println!(
+                "State: {:?}",
+                dispatcher
+                    .dispatch(match choice {
+                        0 => point::Command::GoUp {
+                            id: "MyPoint".to_string(),
+                            v: random
+                        },
+                        1 => point::Command::GoDown {
+                            id: "MyPoint".to_string(),
+                            v: random
+                        },
+                        2 => point::Command::GoLeft {
+                            id: "MyPoint".to_string(),
+                            v: random
+                        },
+                        3 => point::Command::GoRight {
+                            id: "MyPoint".to_string(),
+                            v: random
+                        },
+                        _ => panic!("should not be entered"),
+                    })
+                    .await?
+            );
+        }
+
+        dispatcher
+            .dispatch(point::Command::Register {
+                id: "MyPoint".to_string(),
+            })
+            .await?;
+
+        Ok(())
+    });
+
+    assert_eq!(
+        result,
+        Err(Error::CommandFailed(
+            point::CommandError::AlreadyRegistered("MyPoint".to_string())
+        ))
+    );
+
+    println!("Result: {}", result.unwrap_err());
+}
