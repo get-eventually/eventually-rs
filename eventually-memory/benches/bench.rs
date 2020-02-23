188b99a7a09cffa970769ed646a13c235f7c1cdb
Danilo Cianfrone <danilocianfr@gmail.com>
2020-02-23 09:37:08 +0100

    fix(eventually-memory): change benchmark to use .clone()

--- eventually-memory/benches/bench.rs
+++ eventually-memory/benches/bench.rs
@@ -12,7 +12,7 @@ enum Event {
     C,
 }
 
-fn read_event_stream(store: &MemoryStore<&'static str, Event>, source_id: &'static str) {
+fn read_event_stream(store: MemoryStore<&'static str, Event>, source_id: &'static str) {
     tokio_test::block_on(
         store
             .stream(source_id, 0)
@@ -21,7 +21,7 @@ fn read_event_stream(store: &MemoryStore<&'static str, Event>, source_id: &'stat
     );
 }
 
-fn insert_elements(store: &mut MemoryStore<&'static str, Event>, name: &'static str, num: usize) {
+fn insert_elements(mut store: MemoryStore<&'static str, Event>, name: &'static str, num: usize) {
     tokio_test::block_on(
         store.append(
             name,
@@ -39,27 +39,39 @@ fn insert_elements(store: &mut MemoryStore<&'static str, Event>, name: &'static 
 }
 
 fn benchmark(c: &mut Criterion) {
-    let mut store = MemoryStore::<&'static str, Event>::default();
+    let store = MemoryStore::<&'static str, Event>::default();
 
-    insert_elements(&mut store, "benchtest10", 10);
-    insert_elements(&mut store, "benchtest100", 100);
-    insert_elements(&mut store, "benchtest1_000", 1_000);
-    insert_elements(&mut store, "benchtest10_000", 10_000);
-    insert_elements(&mut store, "benchtest100_000", 100_000);
-    insert_elements(&mut store, "benchtest1_000_000", 1_000_000);
+    insert_elements(store.clone(), "benchtest10", 10);
+    insert_elements(store.clone(), "benchtest100", 100);
+    insert_elements(store.clone(), "benchtest1_000", 1_000);
+    insert_elements(store.clone(), "benchtest10_000", 10_000);
+    insert_elements(store.clone(), "benchtest100_000", 100_000);
+    insert_elements(store.clone(), "benchtest1_000_000", 1_000_000);
 
     c.bench_function("insert events 10", |b| {
-        b.iter(|| insert_elements(black_box(&mut store), black_box("insert_benchtest10"), 10))
+        b.iter(|| {
+            insert_elements(
+                black_box(store.clone()),
+                black_box("insert_benchtest10"),
+                10,
+            )
+        })
     });
 
     c.bench_function("insert events 100", |b| {
-        b.iter(|| insert_elements(black_box(&mut store), black_box("insert_benchtest100"), 100))
+        b.iter(|| {
+            insert_elements(
+                black_box(store.clone()),
+                black_box("insert_benchtest100"),
+                100,
+            )
+        })
     });
 
     c.bench_function("insert events 1_000", |b| {
         b.iter(|| {
             insert_elements(
-                black_box(&mut store),
+                black_box(store.clone()),
                 black_box("insert_benchtest1_000"),
                 1_000,
             )
@@ -69,7 +81,7 @@ fn benchmark(c: &mut Criterion) {
     c.bench_function("insert events 10_000", |b| {
         b.iter(|| {
             insert_elements(
-                black_box(&mut store),
+                black_box(store.clone()),
                 black_box("insert_benchtest10_000"),
                 10_000,
             )
@@ -79,7 +91,7 @@ fn benchmark(c: &mut Criterion) {
     c.bench_function("insert events 100_000", |b| {
         b.iter(|| {
             insert_elements(
-                black_box(&mut store),
+                black_box(store.clone()),
                 black_box("insert_benchtest100_000"),
                 100_000,
             )
@@ -89,7 +101,7 @@ fn benchmark(c: &mut Criterion) {
     c.bench_function("insert events 1_000_000", |b| {
         b.iter(|| {
             insert_elements(
-                black_box(&mut store),
+                black_box(store.clone()),
                 black_box("insert_benchtest1_000_000"),
                 1_000_000,
             )
@@ -97,27 +109,27 @@ fn benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("read stream events 10", |b| {
-        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest10")))
+        b.iter(|| read_event_stream(black_box(store.clone()), black_box("benchtest10")))
     });
 
     c.bench_function("read stream events 100", |b| {
-        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest100")))
+        b.iter(|| read_event_stream(black_box(store.clone()), black_box("benchtest100")))
     });
 
     c.bench_function("read stream events 1_000", |b| {
-        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest1_000")))
+        b.iter(|| read_event_stream(black_box(store.clone()), black_box("benchtest1_000")))
     });
 
     c.bench_function("read stream events 10_000", |b| {
-        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest10_000")))
+        b.iter(|| read_event_stream(black_box(store.clone()), black_box("benchtest10_000")))
     });
 
     c.bench_function("read stream events 100_000", |b| {
-        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest100_000")))
+        b.iter(|| read_event_stream(black_box(store.clone()), black_box("benchtest100_000")))
     });
 
     c.bench_function("read stream events 1_000_000", |b| {
-        b.iter(|| read_event_stream(black_box(&store), black_box("benchtest1_000_000")))
+        b.iter(|| read_event_stream(black_box(store.clone()), black_box("benchtest1_000_000")))
     });
 }
 
