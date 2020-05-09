19d3c1d6dec4c010c912ad49df7abb33356749fe
Danilo Cianfrone <danilocianfr@gmail.com>
2020-05-10 00:24:12 +0200

    hotfix: clean up leftovers of async-trait removal

--- eventually-util/src/command/mod.rs
+++ eventually-util/src/command/mod.rs
@@ -1,18 +1,17 @@
 pub mod dispatcher;
 pub mod r#static;
 
-use async_trait::async_trait;
+use futures::future::BoxFuture;
 
 use eventually_core::aggregate::StateOf;
 use eventually_core::command::{AggregateOf, CommandOf, Handler};
 
-#[async_trait]
 pub trait Dispatcher {
     type CommandHandler: Handler;
     type Error;
 
-    async fn dispatch(
+    fn dispatch(
         &mut self,
         command: CommandOf<Self::CommandHandler>,
-    ) -> Result<StateOf<AggregateOf<Self::CommandHandler>>, Self::Error>;
+    ) -> BoxFuture<Result<StateOf<AggregateOf<Self::CommandHandler>>, Self::Error>>;
 }
