6708cee826a890a30804574719392164bd1aa8a1
Danilo Cianfrone <danilocianfr@gmail.com>
2020-05-15 09:53:11 +0200

    chore: remove useless tags

--- eventually-core/src/lib.rs
+++ eventually-core/src/lib.rs
@@ -1,4 +1,3 @@
-#[deny(missing_docs)]
 pub mod aggregate;
 pub mod repository;
 pub mod store;
--- eventually-util/src/lib.rs
+++ eventually-util/src/lib.rs
@@ -1,6 +1,3 @@
-#[warn(missing_docs)]
 pub mod inmemory;
-#[warn(missing_docs)]
 pub mod optional;
-#[warn(missing_docs)]
 pub mod versioned;
