fc4ada33e6a5baee6b769d85b71d56e1158cab89
Danilo Cianfrone <danilocianfr@gmail.com>
2020-05-24 18:12:42 +0200

    doc(eventually-core): add crate documentation

--- eventually-core/src/lib.rs
+++ eventually-core/src/lib.rs
@@ -1,3 +1,15 @@
+//! Container for the fundamental types and abstraction to support Event Sourcing
+//! and Command-Query Responsibility Segregation patterns.
+//!
+//! ## Note
+//!
+//! Generally, you should not depend from this crate directly, but instead
+//! import the public crate: [`eventually`].
+//!
+//! Please refer to the public crate API documentation for more information.
+//!
+//! [`eventually`]: https://crates.io/crates/eventually
+
 pub mod aggregate;
 pub mod repository;
 pub mod store;
