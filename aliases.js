39bda6f927bcf8505df817adb0ff4e25fadf2dc4
ar3s3ru <ar3s3ru@users.noreply.github.com>
2020-07-20 07:34:21 +0000

    deploy: 216b7341e16f66f0cfcecdae148240afd497e08b

--- aliases.js
+++
@@ -1,6 +0,0 @@
-var ALIASES = {};
-ALIASES["eventually"] = {};
-ALIASES["eventually_core"] = {};
-ALIASES["eventually_postgres"] = {};
-ALIASES["eventually_test"] = {};
-ALIASES["eventually_util"] = {};
