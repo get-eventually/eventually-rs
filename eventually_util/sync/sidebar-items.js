8dc3396b379adbb6d7cb417d26cdb70ac98315b8
ar3s3ru <ar3s3ru@users.noreply.github.com>
2020-08-21 22:42:00 +0000

    deploy: d593dbc4e6e46d63752ea7d875ce812c5e30f5a8

---
+++ eventually_util/sync/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["RwLock","An asynchronous reader-writer lock"]]});
--- source-files.js
+++ source-files.js
@@ -4,5 +4,5 @@ sourcesIndex["eventually_core"] = {"name":"","files":["aggregate.rs","lib.rs","p
 sourcesIndex["eventually_postgres"] = {"name":"","files":["lib.rs","store.rs","subscriber.rs","subscription.rs"]};
 sourcesIndex["eventually_test"] = {"name":"","files":["api.rs","config.rs","lib.rs","order.rs","state.rs"]};
 sourcesIndex["eventually_test_api"] = {"name":"","files":["eventually-test-api.rs"]};
-sourcesIndex["eventually_util"] = {"name":"","dirs":[{"name":"inmemory","files":["mod.rs","projector.rs","store.rs"]}],"files":["lib.rs","optional.rs"]};
+sourcesIndex["eventually_util"] = {"name":"","dirs":[{"name":"inmemory","files":["mod.rs","projector.rs","store.rs"]}],"files":["lib.rs","optional.rs","sync.rs"]};
 createSourceSidebar();
