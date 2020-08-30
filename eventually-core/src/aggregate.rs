1fd81dfe923a5e8d31fd7eab72ebf8827ebabe2b
Danilo Cianfrone <danilocianfr@gmail.com>
2020-08-30 10:55:49 +0200

    refactor(aggregate): remove Clone requirement from AggregateRoot

--- eventually-core/src/aggregate.rs
+++ eventually-core/src/aggregate.rs
@@ -102,14 +102,14 @@ impl<T> AggregateExt for T where T: Aggregate {}
 #[derive(Clone)]
 pub struct AggregateRootBuilder<T>
 where
-    T: Aggregate + Clone,
+    T: Aggregate,
 {
     aggregate: T,
 }
 
 impl<T> From<T> for AggregateRootBuilder<T>
 where
-    T: Aggregate + Clone,
+    T: Aggregate,
 {
     #[inline]
     fn from(aggregate: T) -> Self {
@@ -173,7 +173,7 @@ where
 #[cfg_attr(feature = "serde", derive(Serialize))]
 pub struct AggregateRoot<T>
 where
-    T: Aggregate + Clone + 'static,
+    T: Aggregate + 'static,
 {
     id: T::Id,
     version: u32,
@@ -190,7 +190,7 @@ where
 
 impl<T> PartialEq for AggregateRoot<T>
 where
-    T: Aggregate + Clone,
+    T: Aggregate,
 {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
@@ -200,7 +200,7 @@ where
 
 impl<T> Versioned for AggregateRoot<T>
 where
-    T: Aggregate + Clone,
+    T: Aggregate,
 {
     #[inline]
     fn version(&self) -> u32 {
@@ -210,7 +210,7 @@ where
 
 impl<T> AggregateRoot<T>
 where
-    T: Aggregate + Clone,
+    T: Aggregate,
 {
     /// Returns a reference to the Aggregate [`Id`] that represents
     /// the entity wrapped by this [`AggregateRoot`] instance.
@@ -247,7 +247,7 @@ where
 
 impl<T> AggregateRoot<T>
 where
-    T: Aggregate + Clone,
+    T: Aggregate,
     T::Event: Clone,
     T::State: Clone,
 {
