6ea67d465e7231f0255c7813c4a62df65f71c055
ar3s3ru <ar3s3ru@users.noreply.github.com>
2020-10-04 21:15:05 +0000

    deploy: 2e354902ada4bd3a8b14d28ff52d0313bdc9ec4f

--- implementors/envconfig/traits/trait.Envconfig.js
+++ implementors/envconfig/traits/trait.Envconfig.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["eventually_test"] = [{"text":"impl Envconfig for <a class=\"struct\" href=\"eventually_test/config/struct.Config.html\" title=\"struct eventually_test::config::Config\">Config</a>","synthetic":false,"types":["eventually_test::config::Config"]}];
+implementors["eventually_app_example"] = [{"text":"impl Envconfig for <a class=\"struct\" href=\"eventually_app_example/config/struct.Config.html\" title=\"struct eventually_app_example::config::Config\">Config</a>","synthetic":false,"types":["eventually_app_example::config::Config"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,8 +1,8 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["eventually"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["eventually_app_example"] = {"name":"","files":["api.rs","config.rs","lib.rs","order.rs","state.rs"]};
+sourcesIndex["eventually_app_example_api"] = {"name":"","files":["eventually-app-example-api.rs"]};
 sourcesIndex["eventually_core"] = {"name":"","files":["aggregate.rs","lib.rs","projection.rs","repository.rs","store.rs","subscription.rs","versioning.rs"]};
 sourcesIndex["eventually_postgres"] = {"name":"","files":["lib.rs","store.rs","subscriber.rs","subscription.rs"]};
-sourcesIndex["eventually_test"] = {"name":"","files":["api.rs","config.rs","lib.rs","order.rs","state.rs"]};
-sourcesIndex["eventually_test_api"] = {"name":"","files":["eventually-test-api.rs"]};
 sourcesIndex["eventually_util"] = {"name":"","dirs":[{"name":"inmemory","files":["mod.rs","projector.rs","store.rs"]}],"files":["lib.rs","optional.rs","sync.rs"]};
 createSourceSidebar();
