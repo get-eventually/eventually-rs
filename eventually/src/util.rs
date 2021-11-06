0dfdc3871bce92b731170caa90b465a7f63cc6c2
Daniel Eades <danieleades@hotmail.com>
2021-11-06 12:52:53 +0000

    remove compiler flags from 'util' module

--- eventually/src/util.rs
+++ eventually/src/util.rs
@@ -1,13 +1,6 @@
 //! Collection of utilities that extends or implements some of the traits
 //! found in this crate.
 
-#[deny(
-    clippy::all,
-    missing_docs,
-    unsafe_code,
-    unused_qualifications,
-    trivial_casts
-)]
 pub mod inmemory;
 pub mod optional;
 pub mod sync;
