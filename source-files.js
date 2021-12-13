63c1b767c79a24a6c537248c1bc0db30a7c9e2c4
ar3s3ru <ar3s3ru@users.noreply.github.com>
2021-12-13 10:07:37 +0000

    deploy: 0deeed697ef392937d7167e0f87bee803c268ae6

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["eventually"] = {"name":"","dirs":[{"name":"test","files":["command_handler.rs","store.rs"]}],"files":["aggregate.rs","command.rs","event.rs","lib.rs","metadata.rs","test.rs","version.rs"]};
+sourcesIndex["eventually"] = {"name":"","dirs":[{"name":"test","files":["command_handler.rs","store.rs"]}],"files":["aggregate.rs","command.rs","event.rs","lib.rs","message.rs","metadata.rs","test.rs","version.rs"]};
 sourcesIndex["eventually_postgres"] = {"name":"","files":["lib.rs","store.rs","subscriber.rs","subscription.rs"]};
 sourcesIndex["eventually_redis"] = {"name":"","files":["lib.rs","store.rs","stream.rs","subscriber.rs","subscription.rs"]};
 createSourceSidebar();
