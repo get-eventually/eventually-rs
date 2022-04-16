46fb62e7929c803d2b7bd2131298d726aa00b37b
ar3s3ru <ar3s3ru@users.noreply.github.com>
2022-04-16 21:55:56 +0000

    deploy: dad0944bf3aa52b7d8cff05a26c9ef11d0d60aec

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["bank_accounting"] = {"name":"","files":["application.rs","domain.rs","grpc.rs","lib.rs"]};
-sourcesIndex["eventually"] = {"name":"","dirs":[{"name":"test","files":["command_handler.rs","store.rs"]}],"files":["aggregate.rs","command.rs","event.rs","lib.rs","message.rs","test.rs","version.rs"]};
+sourcesIndex["eventually"] = {"name":"","dirs":[{"name":"aggregate","files":["repository.rs","root.rs"]},{"name":"test","files":["command_handler.rs","store.rs"]}],"files":["aggregate.rs","command.rs","event.rs","lib.rs","message.rs","test.rs","version.rs"]};
 sourcesIndex["eventually_postgres"] = {"name":"","files":["lib.rs","store.rs","subscriber.rs","subscription.rs"]};
 sourcesIndex["eventually_redis"] = {"name":"","files":["lib.rs","store.rs","stream.rs","subscriber.rs","subscription.rs"]};
 createSourceSidebar();
