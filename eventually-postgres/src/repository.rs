acf7c7c421d511a29580f399199078111e5a43bd
Danilo Cianfrone <danilocianfr@gmail.com>
2022-08-25 21:11:12 +0200

    fix: trait constraints on AggregateRepository

--- eventually-postgres/src/repository.rs
+++ eventually-postgres/src/repository.rs
@@ -5,13 +5,7 @@ use eventually::{aggregate, aggregate::Aggregate, version};
 use sqlx::{PgPool, Row};
 
 #[derive(Debug, Clone)]
-pub struct AggregateRepository<T, R, OutT, OutEvt>
-where
-    T: Aggregate + TryFrom<OutT>,
-    R: aggregate::Root<T>,
-    OutT: From<T> + TryFrom<Vec<u8>> + Into<Vec<u8>>,
-    OutEvt: From<T::Event> + Into<Vec<u8>>,
-{
+pub struct AggregateRepository<T, R, OutT, OutEvt> {
     pool: PgPool,
     aggregate_root_type_name: Arc<String>,
     aggregate_type: PhantomData<T>,
@@ -22,10 +16,8 @@ where
 
 impl<T, R, OutT, OutEvt> AggregateRepository<T, R, OutT, OutEvt>
 where
-    T: Aggregate + TryFrom<OutT>,
+    T: Aggregate,
     R: aggregate::Root<T>,
-    OutT: From<T> + TryFrom<Vec<u8>> + Into<Vec<u8>>,
-    OutEvt: From<T::Event> + Into<Vec<u8>>,
 {
     pub fn new(pool: PgPool) -> Self {
         Self {
@@ -47,13 +39,12 @@ where
 #[async_trait]
 impl<T, R, OutT, OutEvt> aggregate::Repository<T, R> for AggregateRepository<T, R, OutT, OutEvt>
 where
-    T: Aggregate + TryFrom<OutT> + TryFrom<Vec<u8>> + Into<Vec<u8>>,
-    T::Event: Into<Vec<u8>>,
+    T: Aggregate,
     R: aggregate::Root<T>,
-    OutT: From<T> + TryFrom<Vec<u8>> + Into<Vec<u8>> + Send + Sync,
+    for<'a> OutT: From<T> + Into<T> + TryFrom<&'a [u8]> + Into<Vec<u8>> + Send + Sync,
     OutEvt: From<T::Event> + Into<Vec<u8>> + Send + Sync,
     // TODO: remove this
-    <OutT as TryFrom<Vec<u8>>>::Error: std::fmt::Debug,
+    for<'a> <OutT as TryFrom<&'a [u8]>>::Error: std::fmt::Debug,
     <T as TryFrom<OutT>>::Error: std::fmt::Debug,
 {
     type Error = sqlx::Error;
@@ -79,8 +70,8 @@ where
         let version_column: i64 = row.try_get("version")?;
         let state_column: Vec<u8> = row.try_get("state")?;
 
-        let deserialized_state = OutT::try_from(state_column).unwrap();
-        let aggregate_root_state = T::try_from(deserialized_state).unwrap();
+        let deserialized_state = OutT::try_from(&state_column).unwrap();
+        let aggregate_root_state: T = deserialized_state.into();
 
         Ok(R::from(aggregate::Context::from_state(
             version_column as version::Version,
