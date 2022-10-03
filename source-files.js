1eee11ba9c67c3d5fe117ebf8274aac52e74d9da
ar3s3ru <ar3s3ru@users.noreply.github.com>
2022-10-03 14:57:48 +0000

    deploy: 06cdbab27b91087e75f39c01a9327f54409f3611

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var sourcesIndex = JSON.parse('{\
-"bank_accounting":["",[],["application.rs","domain.rs","grpc.rs","lib.rs","postgres.rs","serde.rs","tracing.rs"]],\
-"eventually":["",[["aggregate",[],["repository.rs"]],["command",[],["test.rs"]],["event",[],["store.rs"]],["serde",[],["json.rs","prost.rs"]]],["aggregate.rs","command.rs","event.rs","lib.rs","message.rs","serde.rs","tracing.rs","version.rs"]],\
+"bank_accounting":["",[],["application.rs","domain.rs","grpc.rs","lib.rs","serde.rs"]],\
+"eventually":["",[["aggregate",[],["repository.rs"]],["command",[],["test.rs"]],["event",[],["store.rs"]],["serde",[],["json.rs","prost.rs"]]],["aggregate.rs","command.rs","event.rs","lib.rs","message.rs","serde.rs","version.rs"]],\
 "eventually_macros":["",[],["lib.rs"]],\
 "eventually_postgres":["",[],["aggregate.rs","event.rs","lib.rs"]]\
 }');
