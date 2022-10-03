2a201f4179c0552cca14497b33e0de6f73f8a431
ar3s3ru <ar3s3ru@users.noreply.github.com>
2022-10-03 14:52:52 +0000

    deploy: c80cb34391fba410c6119fcfa82b4fca2ad1b187

--- bank_accounting/sidebar-items.js
+++ bank_accounting/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["application",""],["domain",""],["grpc",""],["proto",""],["serde",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["application",""],["domain",""],["grpc",""],["postgres",""],["proto",""],["serde",""],["tracing",""]]};
