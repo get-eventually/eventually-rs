3f402444c498c9f5a835453b284131fee733b08b
ar3s3ru <ar3s3ru@users.noreply.github.com>
2024-02-24 16:17:50 +0000

    deploy: 0ac7441e5c0ed3296152ebc15f91e82661d9d01a

--- trait.impl/eventually/aggregate/trait.Aggregate.js
+++ trait.impl/eventually/aggregate/trait.Aggregate.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"bank_accounting":[["impl Aggregate for <a class=\"struct\" href=\"bank_accounting/domain/struct.BankAccount.html\" title=\"struct bank_accounting::domain::BankAccount\">BankAccount</a>"]]
+"bank_accounting":[["impl <a class=\"trait\" href=\"eventually/aggregate/trait.Aggregate.html\" title=\"trait eventually::aggregate::Aggregate\">Aggregate</a> for <a class=\"struct\" href=\"bank_accounting/domain/struct.BankAccount.html\" title=\"struct bank_accounting::domain::BankAccount\">BankAccount</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
