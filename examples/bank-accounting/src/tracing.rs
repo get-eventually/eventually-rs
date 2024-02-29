9e1c5d52458a1f2f841d38a4042ad8557171d324
Danilo Cianfrone <danilocianfr@gmail.com>
2024-02-29 11:26:04 +0100

    chore(examples/bank-accounting): use opentelemetry-otlp as trace exporter

--- examples/bank-accounting/src/tracing.rs
+++ examples/bank-accounting/src/tracing.rs
@@ -1,12 +1,21 @@
 use anyhow::anyhow;
+use opentelemetry::KeyValue;
+use opentelemetry_sdk::{trace, Resource};
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
 
-pub fn initialize(service_name: &str) -> anyhow::Result<()> {
-    let tracer = opentelemetry_jaeger::new_agent_pipeline()
-        .with_service_name(service_name)
-        .install_simple()
-        .map_err(|e| anyhow!("failed to initialize jaeger tracer: {}", e))?;
+pub fn initialize(service_name: &'static str) -> anyhow::Result<()> {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
+        .with_trace_config(
+            opentelemetry_sdk::trace::config()
+                .with_sampler(trace::Sampler::AlwaysOn)
+                .with_id_generator(trace::RandomIdGenerator::default())
+                .with_resource(Resource::new([KeyValue::new("service.name", service_name)])),
+        )
+        .install_batch(opentelemetry_sdk::runtime::Tokio)
+        .map_err(|e| anyhow!("failed to initialize OTLP tracer: {}", e))?;
 
     let filter_layer = EnvFilter::try_from_default_env()
         .or_else(|_| EnvFilter::try_new("info"))
